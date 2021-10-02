9e011153b4543934ae3ecb01858f8c37c627a8ec
Groogy <groogy@groogy.com>
2021-10-02 12:28:02 +0200

    Toggle color on tool depending on if possible

--- logic/interface/RaiseDykeTool.gd
+++ logic/interface/RaiseDykeTool.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
+
+var cell_under_mouse := Vector2(0, 0)
+
+var _valid_last_render := false
+
+
 func _process(_delta: float) -> void:
-	global_position = Root.map_manager.snap_to_grid(get_global_mouse_position())
+	cell_under_mouse = Root.map_manager.world_to_map(get_global_mouse_position())
+	global_position = Root.map_manager.map_to_world(cell_under_mouse) # Snap to grid
+	if _needs_redraw():
+		update()
 
 
 func _draw() -> void:
-	draw_rect(Rect2(Vector2(0, 0), Vector2(16, 16)), Color.red, false, 1.5, true)
+	_valid_last_render = Root.map_manager.can_build_dyke(cell_under_mouse)
+	
+	var color := Color.green
+	if not _valid_last_render:
+		color = Color.red
+	draw_rect(Rect2(Vector2(0, 0), Vector2(16, 16)), color, false, 1.5, true)
 
+
+func _needs_redraw() -> bool:
+	return _valid_last_render != Root.map_manager.can_build_dyke(cell_under_mouse)
--- logic/managers/MapManager.gd
+++ logic/managers/MapManager.gd
@@ -4,6 +4,7 @@ onready var _water = $WaterMap
 onready var _terrain = $TerrainMap
 
 
+# Queries
 func is_within_bounds(x: int, y: int) -> bool:
 	return x >= 0 and y <= 0 # TODO ADD UPPER BOUND
 
@@ -13,10 +14,24 @@ func is_empty(x: int, y: int) -> bool:
 		and _terrain.get_cell(x, y) == -1 \
 		and _water.get_cell(x, y) == -1
 
+
 func can_have_more_water(x: int, y: int) -> bool:
 	return 	is_within_bounds(x, y) \
 		and (_terrain.is_empty(x, y) or _terrain.is_partial(x, y)) \
 		and _water.get_water_level_at(x, y) < _water.get_max_water_level()
+
+
+func can_build_dyke(cell: Vector2) -> bool:
+	return is_empty(cell.x, cell.y)
+
+
+# Coonversion
+func map_to_world(cell: Vector2) -> Vector2:
+	return _terrain.map_to_world(cell)
+
+
+func world_to_map(pos: Vector2) -> Vector2:
+	return _terrain.world_to_map(pos)
 
 
 func snap_to_grid(pos: Vector2) -> Vector2:
