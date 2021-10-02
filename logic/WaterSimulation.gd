f3f66fd277ecf68edc7c0b1c72e6016dbd6fd959
Groogy <groogy@groogy.com>
2021-10-02 03:30:41 +0200

    Water can now flow

--- logic/WaterSimulation.gd
+++ logic/WaterSimulation.gd
@@ -11,9 +11,21 @@ func _simulate_cell(cell: Vector2) -> void:
 	var manager = get_parent().get_parent()
 	if manager.can_have_more_water(cell.x, cell.y+1):
 		_simulate_gravity(cell)
+	if manager.can_have_more_water(cell.x + 1, cell.y):
+		_simulate_flow(cell, 1)
+	if manager.can_have_more_water(cell.x - 1, cell.y):
+		_simulate_flow(cell, -1)
 
 
 func _simulate_gravity(cell: Vector2) -> void:
 	var map := get_parent()
 	map.add_water_level_at(cell.x, cell.y, -1)
 	map.add_water_level_at(cell.x, cell.y+1, 1)
+
+
+func _simulate_flow(cell: Vector2, dir: int) -> void:
+	var map = get_parent()
+	var target = cell + Vector2(dir, 0)
+	if map.get_water_level_at(target.x, target.y) + 1 < map.get_water_level_at(cell.x, cell.y):
+		map.add_water_level_at(cell.x, cell.y, -1)
+		map.add_water_level_at(target.x, target.y, 1)
--- scenes/MapManager.gd
+++ scenes/MapManager.gd
@@ -4,9 +4,16 @@ onready var _water = $WaterMap
 onready var _terrain = $TerrainMap
 
 
+func is_within_bounds(x: int, y: int) -> bool:
+	return x >= 0 and y <= 0 # TODO ADD UPPER BOUND
+
+
 func is_empty(x: int, y: int) -> bool:
-	return _terrain.get_cell(x, y) == -1 and _water.get_cell(x, y) == -1
+	return is_within_bounds(x, y) \
+		and _terrain.get_cell(x, y) == -1 \
+		and _water.get_cell(x, y) == -1
 
 func can_have_more_water(x: int, y: int) -> bool:
-	return 	_terrain.is_empty(x, y) or _terrain.is_partial(x, y) \
-			and _water.get_water_level_at(x, y) < _water.get_max_water_level()
+	return 	is_within_bounds(x, y) \
+		and (_terrain.is_empty(x, y) or _terrain.is_partial(x, y)) \
+		and _water.get_water_level_at(x, y) < _water.get_max_water_level()
