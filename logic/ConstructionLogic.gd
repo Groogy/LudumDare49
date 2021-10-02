fe98195e2d2b62200c89b4b12ac07dddf97659f4
Groogy <groogy@groogy.com>
2021-10-02 11:06:59 +0200

    Started working on construction

---
+++ logic/ConstructionLogic.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+
+func _on_raise_dyke():
+	pass # Replace with function body.
+
+
+func _on_dig_channel():
+	pass # Replace with function body.
---
+++ logic/ConstructionManager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func can_raise_dyke(cell: Vector2) -> bool:
+	return false
