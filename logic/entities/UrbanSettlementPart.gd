432e5f48560e2b0dca780d3304e2cdb067675947
Groogy <groogy@groogy.com>
2021-10-03 23:56:08 +0200

    Display info on what settlement generates

--- logic/entities/UrbanSettlementPart.gd
+++ logic/entities/UrbanSettlementPart.gd
@@ -1,11 +1,22 @@
 extends "EntityPart.gd"
 
 
-var multiplier := 1.0
+var multiplier := 1.0 setget set_multiplier
 
 
 func _ready() -> void:
 	randomize_settlement_sprite()
+	$Info.visible = false
+	
+
+func _process(_delta: float) -> void:
+	if $Info.visible:
+		_update_info()
+
+
+func set_multiplier(val: float) -> void:
+	multiplier = val
+	$Info/Container/Multiplier.text = "x%.1f" % multiplier
 
 
 func randomize_settlement_sprite() -> void:
@@ -29,4 +40,15 @@ func generated_workers() -> int:
 
 func free_workers() -> int:
 	return $WorkersManager.free_workers
-	
+
+
+func _update_info() -> void:
+	$Info/Container/VBox/Grid/MoneyValue.text = "+%.1f" % generated_income()
+	$Info/Container/VBox/Grid/WorkersValue.text = "+%d" % generated_workers()
+
+func _on_mouse_entered():
+	$Info.visible = true
+
+
+func _on_mouse_exited():
+	$Info.visible = false
