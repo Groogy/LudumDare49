1cb5bebcc6a3fbdd6ab9e85676359d802e9d3d2f
Groogy <groogy@groogy.com>
2021-10-03 14:12:17 +0200

    Can now destroy pumps

--- logic/entities/WindpumpPart.gd
+++ logic/entities/WindpumpPart.gd
@@ -4,6 +4,10 @@ extends "EntityPart.gd"
 export var sucking_power := 16
 
 
+func destroy() -> void:
+	get_parent().queue_free()
+
+
 func _on_sucking() -> void:
 	var children := get_parent().get_children()
 	if children.size() < 2: return
@@ -11,4 +15,3 @@ func _on_sucking() -> void:
 	var random = randi() % children.size()
 	if children[random] != self:
 		children[random].suck(modified)
-
