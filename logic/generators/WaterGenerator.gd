7fe7aa270005875abe36e5af540f670f3e403611
Groogy <groogy@groogy.com>
2021-10-04 15:06:07 +0200

    Fixed issue in water generation at start of the game

--- logic/generators/WaterGenerator.gd
+++ logic/generators/WaterGenerator.gd
@@ -9,6 +9,6 @@ func generate() -> void:
 	var water_level: int = water.inject_water_pos.y
 	for x in range(terrain.map_bounds.position.x, terrain.map_bounds.end.x):
 		var surface: int = terrain.find_surface(x)
-		if surface <= water_level: break
+		if surface <= water_level+1: break
 		for y in range(surface, water_level, -1):
 			water.set_water_level_at(x, y, Const.MAX_WATER_LEVEL)
